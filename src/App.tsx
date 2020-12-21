b7220fd0aa70619ccd12b6ab8280b5fc2dedee8f
buingoctai <buingoctai994@gmail.com>
2020-12-21 22:08:10 +0700

    init demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import {BrowserRouter as Router, Switch, Route,Link} from "react-router-dom";
 import logo from './logo.svg';
-import './App.css';
+// import About from "./About";
+const About = lazy(() => import('./About'));
+// import Home from "./Home";
+const Home = lazy(() => import('./Home'));
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+    <Switch>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
+    </Suspense>
+   
+  </Router>
+);
+
 
 export default App;
+
+
+// offline
+// cache
+// workbox
+// code spliting
+// add to home scren A2HS
+// mainfest
