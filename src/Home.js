5e194aef31b0fb990e35012ed46495cc8cf32680
buingoctai <buingoctai994@gmail.com>
2020-12-22 22:05:48 +0700

    update path

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 const Home = () => {
   useEffect(() => {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/js/sw.js').then(function () {
+      navigator.serviceWorker.register('/sw.js').then(function () {
         console.log('Service Worker Registered');
       });
     }
